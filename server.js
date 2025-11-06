74a70e249a30c7a9cb48947c02d4cafdfbfd11ad
Robin-Wynn <wynnrobin95@gmail.com>
2025-11-06 11:42:49 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,6 @@
+// Build server
+const express = require('express')
+const server = express()
+const PORT = process.env.PORT || 1995
+
+server.listen(PORT, ()=> console.log(`PORTY over here (￣y▽￣)╭ Ohohoho.....`))
