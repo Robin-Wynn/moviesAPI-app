1884f895cdfd05781f8b08912e43c2c9067586f9
Robin-Wynn <wynnrobin95@gmail.com>
2025-11-07 11:04:04 -0600

    create sort and findById methods

--- daos/common/daoCommon.js
+++ daos/common/daoCommon.js
@@ -25,7 +25,55 @@ const daoCommon = {
                 }
             }
         )
-    }
+    },
+
+	findById: (res, table, id)=> {
+	
+		connect.query(
+			`SELECT * FROM ${table} WHERE ${table}_id = ${id};`,
+			(error, rows)=> {
+				if (!error) {
+					res.json(...rows)
+				} else {
+					console.log(`Dao Error: ${error}`)
+					res.json({
+						"message": 'error',
+						"table": `${table}`,
+						'error': error
+					})
+				}
+			}
+
+		)
+	
+	},
+
+	sort: (res, table, sorter)=> {
+
+		connect.query(
+			`SELECT * FROM ${table} ORDER BY ${sorter};`,
+		
+			(error, rows)=> {
+				if (!error) {
+					if (rows.length === 1) {
+						res.json(...rows)
+					} else {
+						res.json(rows)
+					}
+				} else {
+					console.log(`Dao Error: ${error}`)
+					res.json({
+						"message": 'error',
+						"table": `${table}`,
+						'error': error
+					})
+				}
+			}
+
+		)
+	} 
+
+
 }
 
 module.exports = daoCommon
--- routes/api/movieRoute.js
+++ routes/api/movieRoute.js
@@ -2,8 +2,18 @@ const express = require('express')
 const router = express.Router()
 const { movieDao: dao } = require('../../daos/dao')
 
+// http://localhost:1995/api/movie
 router.get('/', (req, res)=> {
-    dao.findAll(req, res, dao.table)
+    dao.findAll(req, res, dao.table) //req would be needed for pagination
+})
+
+router.get('/sort/:sorter', (req, res)=> {
+    dao.sort(res, dao.table, req.params.sorter)
+})
+
+// http://localhost:1995/api/movie/1
+router.get('/:id', (req, res)=> {
+    dao.findById(res, dao.table, req.params.id)
 })
 
 module.exports = router
