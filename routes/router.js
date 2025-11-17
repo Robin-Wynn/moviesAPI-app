3839831be9c6cf10d930260f3c52f2ffb2051c08
Robin-Wynn <wynnrobin95@gmail.com>
2025-11-17 15:30:03 -0600

    forms ejs 📃

--- routes/router.js
+++ routes/router.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const router = express.Router()
+const con = require('../config/dbconfig')
 const PORT = process.env.PORT || 1995
 
 // Home Page => http://localhost:1995
-router.get('/', (req, res)=> {
+router.get('/', async (req, res) => {
+
     res.render('pages/home', {
         title: 'Movie App Home',
-        name: 'Robin\'s Movie App🎬'
+        name: 'Robin\'s Movie App🎬',
     })
+
 })
 
 // Actor-Form => http://localhost:1995/actor-form
@@ -17,6 +20,50 @@ router.get('/actor-form', (req, res)=> {
         name: 'actor-form'
     })
 })
+// movies-form => http://localhost:1995/movies-form
+router.get('/movies-form', (req, res)=> {
+    res.render('pages/movies-form', {
+        title: 'movies-form',
+        name: 'movies-form'
+    })
+})
+// genre-form => http://localhost:1995/genre-form
+router.get('/genre-form', (req, res)=> {
+    res.render('pages/genre-form', {
+        title: 'genre-form',
+        name: 'genre-form'
+    })
+})
+// movies-form => http://localhost:1995/movies-form
+router.get('/movies-form', (req, res)=> {
+    res.render('pages/movies-form', {
+        title: 'movies-form',
+        name: 'movies-form'
+    })
+})
+
+// // Forms 
+// router.get('/pages', (req, res)=> {
+//     res.json({
+//         'Movies Form': `http://localhost:${PORT}/pages/movies-form`,
+//         'Actor Form': `http://localhost:${PORT}/pages/actor-form`,
+//         'Director Form': `http://localhost:${PORT}/pages/director-form`,
+//         'Production Company Form': `http://localhost:${PORT}/productionCompany-form`,
+//         'Genre Form': `http://localhost:${PORT}/genre-form`
+//     })
+// })
+
+// const formEndpoints = [
+//     'actor-form',
+//     'director-form',
+//     'genre-form',
+//     'movies-form',
+//     'productionCompany-form'
+// ]
+
+// formEndpoints.forEach(formEndpoint => {
+//     router.use(`/pages/${formEndpoint}`, require(`../views/pages/${formEndpoint}`))
+// })
 
 // root route 
 router.get('/api', (req, res)=> {
